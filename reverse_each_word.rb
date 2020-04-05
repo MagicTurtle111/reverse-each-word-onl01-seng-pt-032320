6076dbffdc0a9ac21fe91732daf2cee179f10d8d
Joshua Williams <j.o-williams@outlook.com>
2020-04-05 03:07:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
 reversed_sentence = sentence.split
 
 reversed_sentence.collect { |item|
-  item.reverse
+  item.reverse!
 }
 sentence = reversed_sentence.join(" ")
 sentence
